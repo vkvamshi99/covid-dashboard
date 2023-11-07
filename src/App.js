c371aace3ca31eb6ac9411d5d9cab5c793113f7b
vkvamshi99 <vamshi928@gmail.com>
2023-11-07 20:10:28 +0530

    finalcomit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import Card from "./SummaryCard";
+// Imports end
 
 function App() {
+  // App component starts here
+  const locationList = [
+    { value: "AB", label: "Alberta" },
+    { value: "BC", label: "British Columbia" },
+    { value: "MB", label: "Manitoba" },
+    { value: "NB", label: "New Brunswick" },
+    { value: "NL", label: "Newfoundland and Labrador" },
+    { value: "NT", label: "Northwest Territories" },
+    { value: "NS", label: "Nova Scotia" },
+    { value: "NU", label: "Nunavut" },
+    { value: "ON", label: "Ontario" },
+    { value: "PE", label: "Prince Edward Island" },
+    { value: "QC", label: "Quebec" },
+    { value: "SK", label: "Saskatchewan" },
+    { value: "YT", label: "Yukon" },
+  ];
+  const [activeLocation, setActiveLocation] = useState("AB");
+  const [lastUpdated, setlastUpdated] = useState("");
+  const [summaryData, setSummaryData] = useState({});
+
+  const baseUrl = "https://api.opencovid.ca";
+
+  useEffect(() => {
+    getSummaryData();
+    getVersion();
+  }, [activeLocation]);
+
+  const getVersion = async () => {
+    const res = await fetch(`${baseUrl}/version`);
+    const data = await res.json();
+
+    setlastUpdated(data.timeseries);
+  };
+
+  const getSummaryData = async (location) => {
+    if (activeLocation === "canada") {
+      return;
+    }
+    let res = await fetch(`${baseUrl}/summary?loc=${activeLocation}`);
+    let resData = await res.json();
+    let summaryData = resData.data[0];
+    let formattedData = {};
+
+    Object.keys(summaryData).map(
+      (key) => (formattedData[key] = summaryData[key].toLocaleString())
+    );
+    console.log(formattedData);
+    setSummaryData(formattedData);
+  };
+
+  //return statement goes below this
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COVID 19 Dashboard </h1>
+      <div className="dashboard-container">
+        <div className="dashboard-menu ">
+          <Select
+            options={locationList}
+            onChange={(selectedOption) =>
+              setActiveLocation(selectedOption.value)
+            }
+            defaultValue={locationList.filter(
+              (options) => options.value === activeLocation
+            )}
+            className="dashboard-select"
+          />
+          <p className="update-date">Last Updated : {lastUpdated}</p>
+        </div>
+
+        <div className="dashboard-summary">
+          <Card title="Total Cases" value={summaryData.cases} />
+          <Card title="Total Tests" value={summaryData.tests_completed} />
+          <Card title="Total Deaths" value={summaryData.deaths} />
+          <Card
+            title="Total Vaccinated"
+            value={summaryData.vaccine_administration_total_doses}
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/SummaryCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <div className="summary-card">
+      <h2>{props.title}</h2>
+      <p>{props.value}</p>
+    </div>
+  );
+}
